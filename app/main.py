ff7f1324a4dce17d7c99659d7255e4d4c64dc33a
akshatkumar0327 <159109581+akshatkumar0327@users.noreply.github.com>
2024-04-28 00:44:35 +0530

    init

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+# from app.routes.routes import router as student_router
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins for simplicity, replace with your frontend URL in production
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS"],  # Include OPTIONS method
+    allow_headers=["*"],
+)
+
+# app.include_router(student_router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
